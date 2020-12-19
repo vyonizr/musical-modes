29990a31010f3513bbc36d0fbabcb0ee829dee3f
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-12-19 18:32:23 +0700

    add slash on SITE_IMAGE

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,4 +2,4 @@ export const SITE_NAME = 'Musical Modes by vyonizr'
 export const SITE_TITLE = 'Musical Modes'
 export const SITE_DESCRIPTION =
   'A website to show modes chords with selected key'
-export const SITE_IMAGE = 'assets/images/musical_modes_og.jpg'
+export const SITE_IMAGE = '/assets/images/musical_modes_og.jpg'
