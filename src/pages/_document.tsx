88886151f004c9266b69681298b680ff5d5498cf
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-12-20 08:23:58 +0700

    setup PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,8 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta charSet='utf-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='theme-color' content='#161a1d' />
           <meta
             name='keywords'
             content='music theory mode scale scale key vyonizr'
@@ -39,6 +41,20 @@ class MyDocument extends Document {
           <meta name='twitter:description' content={SITE_DESCRIPTION} />
           <meta property='twitter:image' content={SITE_IMAGE} />
 
+          <link rel='manifest' href='/manifest.json' />
+          <link
+            href='/favicon-16x16.png'
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+          />
+          <link
+            href='/favicon-32x32.png'
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+          />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap'
